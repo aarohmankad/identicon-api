83ab64dc37a459004b2b17a237faad08b25fef46
Aaroh Mankad <aarohmankad@gmail.com>
2017-07-08 16:04:48 -0700

    upgrade jdenticon version

--- routes/identicons/generate.js
+++ routes/identicons/generate.js
@@ -5,11 +5,18 @@ const
 
 module.exports = (router) => {
 	router.get('/:hash/:size', (req, res) => {
+
 		let
 			hash = Hash.sha256().update(req.params.hash).digest('hex'),
-			size = req.params.size;
+			size = parseInt(req.params.size);
 
-		let identicon = jdenticon.toSvg(hash, size);
+		let identicon;
+
+		if (req.query.format == 'png') {
+			identicon = jdenticon.toPng(hash, size);
+		} else {
+			identicon = jdenticon.toSvg(hash, size);
+		}
 
 		return res.send(identicon);
 	});
